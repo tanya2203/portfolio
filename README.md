41af8bc8847269e6146d85c3ec246d7207c82665
Tanya verma <tanyaverma@Tanyas-MacBook-Air.local>
2023-04-01 17:49:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+
+
+
+## Built With
+
+- HTML
+- CSS
+- JavaScripts
+
+## Live Demo (Link for My portfolio)
+
+[Live Demo Link](https://portfolio-website-nine-pink.vercel.app/)
+
+
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse team for guidance
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ projectData.js
@@ -0,0 +1,41 @@
+const data = {
+  projects: [
+    {
+      key: 11,
+      projectName: 'Break',
+      projectImageMobile: './projects/break.png',
+      projectImageDesktop: './projects/break.png',
+      companyName: 'MICROVERSE',
+      projectPosition: 'Full Stack Dev',
+      projectDate: 2022,
+
+      projectDescription: 'An open Platform where students can find Proffesional experts and undiscovered voices sharing their journeys for a better direction and guidance.',
+      projectTechnologies: ['HTML', 'CSS', 'Javascript', 'Firebase'],
+      projectTextMobile: 'Multiple functionalities like User authentication (Sign in or login), upload videos, download videos made using ReactJS',
+
+      seeLive: './images/seeLive.png',
+      seeSource: './images/seeSource.png',
+      seeLivePage: 'https://videoapp-yt-de6c0.firebaseapp.com/login',
+      seeSourceCode: 'https://github.com/tanya2203/Break/',
+    },
+    {
+      key: 10,
+      projectName: 'AutoStart',
+      projectImageMobile: './projects/autostart.png',
+      projectImageDesktop: './projects/autostart.png',
+      companyName: 'MICROVERSE',
+      projectPosition: 'Frontend End Dev',
+      projectDate: 2022,
+      projectDescription: 'The Ruby on Rails capstone project Budget App is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
+      projectTechnologies: ['CSS', 'HTML', 'JavaScript', 'Firebase'],
+      projectTextMobile: 'The website put forward the finest range of Auto Self Starter Parts where customer can search according to the categories and order through payment gateway.',
+      projectTextDesktop:
+        'The website put forward the finest range of Auto Self Starter Parts where customer can search according to the categories and order through payment gateway.',
+      seeLive: './images/seeLive.png',
+      seeSource: './images/seeSource.png',
+      seeLivePage: 'https://github.com/tanya2203/Autostart',
+      seeSourceCode: 'https://github.com/tanya2203/Autostart',
+    },
+  ],
+};
+export default data;
